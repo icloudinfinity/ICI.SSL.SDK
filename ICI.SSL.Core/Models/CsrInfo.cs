bf29cd2a18bb112ecbff9d45870bd95cf54445e5
Ray Consulting <rayconsulting@shopeneur.com>
2022-07-13 11:14:50 -0400

    updated csrinfo

--- ICI.SSL.Core/Models/CsrInfo.cs
+++ ICI.SSL.Core/Models/CsrInfo.cs
@@ -6,41 +6,12 @@ namespace ICI.SSL.Core
 {
     public class CsrInfo
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Country")]
-        [MaxLength(50)]
         public string countryName { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "City")]
-        [MaxLength(50)]
         public string locality { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "State/Region")]
-        [MaxLength(50)]
         public string state { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Organization")]
-        [MaxLength(100)]
         public string organization { get; set; }
-
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Organization Unit")]
-        [MaxLength(100)]
         public string organizationUnit { get; set; }
-
-        [Required]
-        [Display(Name = "Subscription Id")]
         public int subscriptionId { get; set; }
     }
 }
